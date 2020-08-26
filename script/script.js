8c2c71d087ec5e9ba5f0d11c822bf644673fc16e
Steven Schaab <stevenes83@gmail.com>
2020-08-25 22:26:53 -0500

    updated read me

--- script/script.js
+++ script/script.js
@@ -1,17 +1,20 @@
 $("#projectsBtn").on("click", function() {
     $("#projects").show();
+    $("#pages").show();
     $("#contacts").hide();
     $("#resume").hide();
     })
 
 $("#contactsBtn").on("click", function() {
     $("#contacts").show();
+    $("#pages").show();
     $("#projects").hide();
     $("#resume").hide();
     })
 
 $("#resumeBtn").on("click", function() {
     $("#resume").show();
+    $("#pages").show();
     $("#contacts").hide();
     $("#projects").hide();
     })

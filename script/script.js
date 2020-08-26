faaa0dd292e19183e119beae9bd05b6895d6f154
Steven Schaab <stevenes83@gmail.com>
2020-08-25 19:19:11 -0500

    still needs resume

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
-$("#steven").on("click", function() {
-    
+$("#projectsBtn").on("click", function() {
+    $("#projects").show();
+    $("#contacts").hide();
+    $("#resume").hide();
+    })
+
+$("#contactsBtn").on("click", function() {
+    $("#contacts").show();
+    $("#projects").hide();
+    $("#resume").hide();
+    })
+
+$("#resumeBtn").on("click", function() {
+    $("#resume").show();
+    $("#contacts").hide();
+    $("#projects").hide();
     })
